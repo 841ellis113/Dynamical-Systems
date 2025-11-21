b0b9d026c5b57022df894cb013b2055546df6a99
Michael Ellis <m.ellis841113@gmail.com>
2025-11-21 23:27:03 +0000

    Refactor comments and fix typos in feigenbaum.py
    Corrected the description of the Lyapunov exponent and its implications. Updated comments for clarity and fixed minor typos.

--- feigenbaum.py
+++ feigenbaum.py
@@ -3,8 +3,8 @@ import matplotlib.pyplot as plt
 
 # A program that graphs the logistic map for values of the parameter r between 0 and 4. Above 4
 #the logistic map is ill defined. It overlays the lyapunov exponent calculated for each value
-#of r. A positive value of the lyapunov exponent at r = r0 indicates that parameter is well
-#behaved or non-chaotic. For negative exponents, this indicates that the particulr r value
+#of r. A negative value of the lyapunov exponent at r = r0 indicates that parameter is well
+#behaved or non-chaotic. For positive exponents, this indicates that the particulr r value
 #is chaotic in nature. The gradient of the Lyapunov exponent graph indicates the sensitivity
 #the system is to changes in the parameter. A large absolute value of the exponent means that
 #the system predictability is more sensitve to small changes in initial conditions. A smaller
@@ -36,7 +36,7 @@ def lyapunov(r,x,iterations):
     lyapunov = round((sum(log)/len(log)),2)
     return lyapunov
 
-#calculate the values of the logistical map with an initial x0 and parameter r, only capturing dat
+#calculate the values of the logistical map with an initial x0 and parameter r, only capturing data
 #after the first 5000 iterations to remove transient results
 def logistic_map_values(r,x,iterations):
     x_values   = []
@@ -69,7 +69,7 @@ def range_lyapunov(x0,iterations):
     plt.show()
 
 #finally overlay both graphs onto the same axis. We can see that at arounr r = 3.57, the value of
-    #the Lyapunov expon
+#the Lyapunov exponent becomes posititve and a chaos begins
 def Overlay_plot(x0,iterations):
     lyapunov_values = []
     r_values        = []
@@ -89,10 +89,10 @@ def Overlay_plot(x0,iterations):
     ax2.plot(r_values,lyapunov_values, color='tab:blue', linewidth='0.7')
     ax2.set_ylabel('Lyapunov Exponent', color='tab:blue')
     ax2.tick_params(axis='y',labelcolor='tab:blue')
-    #ax2.axhline(0,color = 'red', linestyle='--', linewidth='0.8', label='$\\lambda$ = 0')
     ax2.axvline(3.57, color = 'red', linestyle='--', linewidth='0.8', label = 'r =  3.57')
     plt.suptitle('Overlay of Logistic Map and Lyapunov Exponent at Different Values of r')
     plt.show()
+
 
 
 
