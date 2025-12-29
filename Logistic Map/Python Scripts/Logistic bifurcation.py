efa274873e1764d996b75938b932255d95ec2c91
Michael Ellis <m.ellis841113@gmail.com>
2025-12-29 20:50:16 +0000

    Add files via upload

--- Logistic Map/Python Scripts/Logistic bifurcation.py
+++ Logistic Map/Python Scripts/Logistic bifurcation.py
@@ -45,3 +45,40 @@ def graph_logistic(r1,r2,x0,iterations,transients):
     plt.ylabel('Orbit Values (x)')
     plt.grid(True,which='both',linewidth=0.5,color='grey',linestyle='--')
     plt.show()
+
+'''
+Calculate the Shannon Entropy of the logistic map for a given value of the parameter r
+'''
+
+def logistic_shannon_entropy(r,x,iterations,transients,bins,base='e'):
+    rdata, xdata = logistic_map_values(r,x,iterations,transients)
+    data   = numpy.array(xdata,float)
+    allocation = numpy.floor(data*bins)
+    unique, counts = numpy.unique(allocation, return_counts=True)
+    prob = counts/sum(counts)
+    prob = prob[prob>0]
+    if base == 'e':
+        entropy_i = prob*numpy.log(prob)
+        entropy   = -1*sum(entropy_i)
+    elif base == 2:
+        entropy_i  = prob*numpy.log2(prob)
+        entropy   = -1*sum(entropy_i)
+    return entropy
+
+def shannon_entropy_range(r1,r2,x,iterations,transients,bins,base='e'):
+    r_range = numpy.linspace(r1,r2,2500)
+    r_values = []
+    entropy = []
+    for r in r_range:
+        r_values.append(r)
+        ent  = logistic_shannon_entropy(r,x,iterations,transients,bins,base)
+        entropy.append(ent)
+    plt.scatter(r_values,entropy, marker='.', s=3,color='red')
+    plt.grid(True, which = 'both', linestyle ='--', linewidth=1, color='grey')
+    plt.title(f'Variation of Shannon Entropy for Logistic Map with r for {bins} Bins')
+    plt.xlim(0,4)
+    plt.xlabel('Parameter (r)')
+    plt.ylabel('Shannon Entropy')
+    plt.show()
+
+        
